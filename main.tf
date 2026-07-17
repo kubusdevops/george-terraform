351055a50ac1eee4f5b835da0ceeac3402e27526
George Akwari <devops4george@gmail.com>
2026-07-16 20:45:12 -0500

    created ec2 using terraform

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "george_appn" {
   ami             = "ami-01edba92f9036f76e"
-  instance_type   = "t2.micro"
+  instance_type   = "t3.micro"
   key_name        = "kubuskey"
   security_groups = [aws_security_group.george_sg.name]
 
