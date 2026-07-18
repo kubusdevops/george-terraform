5f3f532be9c7850f6e2ab4b46785d4f511e2a9fe
George Akwari <devops4george@gmail.com>
2026-07-18 04:39:56 -0500

    managed state file in s3 bucket

--- provider.tf
+++ provider.tf
@@ -4,9 +4,16 @@ terraform {
     aws = {
       source  = "hashicorp/aws"
       version = "~>4.0"
-    }
+    
   }
 }
+backend "s3" {
+  bucket = "kubus-terraform-state"
+  key    = "terraform.tfstate"
+  region = "us-east-1"
+}
+}
+
 provider "aws" {
   region = "us-east-1"
 }
